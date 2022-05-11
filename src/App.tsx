e72297369f9ff83f65302487d543384cc36c7673
Simmigon Flagg <simmigon.flagg@gmail.com>
2022-05-11 14:17:24 -0400

    Errors in Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Amplify } from 'aws-amplify';
-import  { useEffect } from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
@@ -10,13 +9,13 @@ Amplify.configure(awsExports);
 
 const App = () => {
 
-  useEffect(() => {
-    fetch('https://a17lfk040i.execute-api.us-east-1.amazonaws.com/staging/dashboard')
-    // fetch('http://localhost:3001/dashboard')
-      .then(response => response.json())
-      .then(data => console.log(data));
+  // useEffect(() => {
+  //   fetch('https://a17lfk040i.execute-api.us-east-1.amazonaws.com/staging/dashboard')
+  //   // fetch('http://localhost:3001/dashboard')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data));
 
-  }, []);
+  // }, []);
 
   return (
     <Authenticator>
