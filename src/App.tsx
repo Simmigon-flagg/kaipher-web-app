cb635bfa6e1e06f9e537dbb204df5a02ba8cbd83
Simmigon Flagg <simmigon.flagg@gmail.com>
2022-05-11 14:11:30 -0400

    Updating App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
+import  { useEffect } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from './aws-exports';
+
+
+Amplify.configure(awsExports);
+
+const App = () => {
+
+  useEffect(() => {
+    fetch('https://a17lfk040i.execute-api.us-east-1.amazonaws.com/staging/dashboard')
+    // fetch('http://localhost:3001/dashboard')
+      .then(response => response.json())
+      .then(data => console.log(data));
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div >
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <h1>My Todos App</h1>
+
+          <br />
+        </div>
+      )}
+
+
+    </Authenticator>
   );
 }
 
